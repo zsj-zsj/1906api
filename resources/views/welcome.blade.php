66a7f5b69ae813d667ad10d7a2a90cd4ce34debe
zsj-zsj <2211045409@qq.com>
2020-02-10 07:36:31 +0000

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravellaravel
+                    Laravel
                 </div>
 
                 <div class="links">
